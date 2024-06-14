e1e04930691e36a5dcaa92b853196103c305354d
Karan Gaikwad <57894733+Karan-512@users.noreply.github.com>
2024-06-14 17:24:17 -0400

    Update InventoryController.java

--- src/main/java/com/acks/task1/controllers/InventoryController.java
+++ src/main/java/com/acks/task1/controllers/InventoryController.java
@@ -20,7 +20,7 @@ import com.acks.task1.models.Inventory;
 
 
 @RestController
-@RequestMapping("/inventory-app")
+@RequestMapping("/inventory")
 public class InventoryController {
 
     public  List<Inventory> inventoryList = new ArrayList<>(Arrays.asList(
